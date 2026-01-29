5fc8a19ebc27624d3e44d68428765e0179309c2b
Alexandr <rfst@mail.ru>
2026-01-29 21:01:47 +0700

    create AbstractMonoEntity and MonoLink

---
+++ Assets/Scripts/Core/Ecs/AbstractMonoEntity.cs
@@ -0,0 +1,45 @@
+﻿using Leopotam.Ecs;
+using UnityEngine;
+using Zenject;
+
+namespace Scripts.Core.Ecs
+{
+    public abstract class AbstractMonoEntity : MonoBehaviour
+    {
+        [Inject] private EcsStartup _ecsStartup;
+
+        private EcsEntity _ecsEntity;
+
+        public EcsEntity EcsEntity => _ecsEntity;
+
+        [Inject]
+        private void PostInject()
+        {
+            _ecsEntity = _ecsStartup.World.NewEntity();
+            PostInjectInternal();
+        }
+
+        private void Awake()
+        {
+            ConfigureEntity(ref _ecsEntity);
+            OnAwake();
+        }
+
+        private void OnDestroy()
+        {
+            if (_ecsEntity.IsWorldAlive())
+            {
+                _ecsEntity.Destroy();
+            }
+            OnDestroyInternal();
+        }
+
+        protected virtual void PostInjectInternal() { }
+
+        protected virtual void OnDestroyInternal() { }
+
+        protected virtual void OnAwake() { }
+
+        protected abstract void ConfigureEntity(ref EcsEntity ecsEntity);
+    }
+}
---
+++ Assets/Scripts/Core/Ecs/MonoLink.cs
@@ -0,0 +1,24 @@
+﻿using Leopotam.Ecs;
+using UnityEngine;
+
+namespace Scripts.Core.Ecs
+{
+    public class MonoLink<T> : MonoBehaviour
+        where T : struct
+    {
+        public T Component;
+        public EcsComponentRef<T> EcsCompRef { get; private set; }
+
+        public EcsComponentRef<T> ApplyComponentOnEntity(ref EcsEntity entity)
+        {
+            PrepareComponent(ref Component);
+            entity.Replace(Component);
+            EcsCompRef = entity.Ref<T>();
+            OnEntityConfigured(ref entity);
+            return EcsCompRef;
+        }
+
+        protected virtual void PrepareComponent(ref T targetComponent) { }
+        protected virtual void OnEntityConfigured(ref EcsEntity entity) { }
+    }
+}
