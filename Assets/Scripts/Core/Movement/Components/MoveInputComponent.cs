9152df8bcafae7f8e772d4c2792dfb67c417cf5b
Alexandr <rfst@mail.ru>
2026-02-20 18:57:47 +0700

    create MoveInputComponent

---
+++ Assets/Scripts/Core/Movement/Components/MoveInputComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core.Movement.Components
+{
+    [Serializable]
+    public struct MoveInputComponent
+    {
+        public Vector2 Value { get; set; }
+    }
+}
